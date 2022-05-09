f106daee986fd2c286fdcacc35d205ea5575b23a
White Nguyen <whitenguyen0909@gmail.com>
2022-05-08 20:14:01 -0600

    useEffect hook and make it pull info from mock site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, setUser] = useState({});
+
+  const changeHandler = (event) => {
+    // console.log("target", event.target)
+    // console.log("id", event.target.id)
+    // console.log("type", event.target.type)
+    // console.log("value", event.target.value)
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+
+  console.log("render", user)
+  useEffect(() => {
+    console.log("useEffect hook in action")
+    async function loadUsers() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users/2"
+      );
+      const userFromAPI = await response.json();
+      console.log("setUser then render", userFromAPI)
+      setUser(userFromAPI);
+    }
+    loadUsers();
+  }, []);
+  /*
+  //more concise syntax using the Promise interface, but they say it is old:
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((response) => response.json())
+      .then(setUser);
+  }, []); //not setUser(response) ? 
+  */
+  // console.log("user", user)
+  // console.log(user.id)
+  // console.log("key username", user.username)
+  // console.log("key wrongusername", user.wrongusername)
+  // console.log(user.email2)
+
+  if (user.id) {
+    return (
+      <form name="profileEdit">
+        <label htmlFor="username2">
+          User Name:
+          <input
+            id="username2"
+            name="wrongusername"
+            type="text"
+            value={user.username}
+            onChange={changeHandler}
+          />
+        </label>
+        <label htmlFor="email2">
+          User Name:
+          <input
+            id="email2"
+            name="wrongemail"
+            type="email"
+            value={user.email}
+            onChange={changeHandler}
+          />
+        </label>
+      </form>
+    );
+  }
+  // console.log("Loadddddddddddddddddddddding");
+  return "Loadddddddddddddddddddddding";
 }
 
 export default App;
