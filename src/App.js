ac34e5da72e19132dcaa100421d254344cad3961
White Nguyen <whitenguyen0909@gmail.com>
2022-05-09 16:53:31 -0600

    set up for 24-4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import ProfileEdit from "./ProfileEdit";
-import ToDos from "./ToDos";
+
 import ClickCounter from "./ClickCounter";
 
-function App() {
-  const [userId, setUserID] = useState(1);
+import ProfileEdit from "./ProfileEdit";
+import ToDos from "./ToDos";
 
-  const userIds = [1, 2, 3, 4];
+import AlbumList from "./AlbumList";
+import UserList from "./UserList";
+
+function App() {
+  // const [userId, setUserID] = useState(1);
+  // const userIds = [1, 2, 3, 4];
   // console.log(userId)
+
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
+  // Load data from https://jsonplaceholder.typicode.com/albums?userId=${user.id}
+
   return (
     <div className="App">
       {/* <ClickCounter /> */}
-      {userIds.map((id) => (
+      {/* {userIds.map((id) => (
         <button key={id} onClick={() => setUserID(id)}>
           User ID {id}
         </button>
-      ))}
-      <h2>User ID {userId}</h2>
+      ))} */}
+      {/* <h2>User ID {userId}</h2> */}
       {/* <ProfileEdit userID={userId}/> */}
-      <ToDos />
+      {/* <ToDos /> */}
+      <div className="left column">
+        <UserList users={users} setCurrentUser={setCurrentUser} />
+      </div>
+      <div className="right column">
+        <AlbumList user={currentUser} />
+      </div>
     </div>
   );
 }
---
+++ src/UserList.js
@@ -0,0 +1,4 @@
+
+function UserList() {};
+
+export default UserList;
